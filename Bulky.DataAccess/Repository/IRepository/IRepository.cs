ceb48206faaf4c6fa5a7e183c15150855f949144
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-10-01 14:48:35 +0300

    Implement Repository Interface

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        //Purpose: Retrieves all records from the data store for the given entity type.
+        //Return Type: IEnumerable<T> represents a collection of T entities.
+        //Use Case: Useful for returning all rows in a database table, such as getting a list of all products.
+        IEnumerable<T> GetAll();
+        //Purpose: Retrieves a single entity from the data store that matches the specified filter condition.
+        //Parameter:Expression<Func<T, bool>> filter: Represents a LINQ expression used to filter records, such as product => product.Id == 5.
+        //Return Type: T — Returns a single entity that matches the criteria or null if no match is found.
+        //Use Case: Fetch a single record based on a specific condition, such as getting a product by ID or by name.
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+    }
+}
