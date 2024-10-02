01566f88d10cb4f128b7e504714c2d2d82143623
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-10-02 21:16:03 +0300

    Loading Navigation Properties

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -12,12 +12,12 @@ namespace Bulky.DataAccess.Repository.IRepository
         //Purpose: Retrieves all records from the data store for the given entity type.
         //Return Type: IEnumerable<T> represents a collection of T entities.
         //Use Case: Useful for returning all rows in a database table, such as getting a list of all products.
-        IEnumerable<T> GetAll();
+        IEnumerable<T> GetAll(string? includeProperties = null);
         //Purpose: Retrieves a single entity from the data store that matches the specified filter condition.
         //Parameter:Expression<Func<T, bool>> filter: Represents a LINQ expression used to filter records, such as product => product.Id == 5.
         //Return Type: T — Returns a single entity that matches the criteria or null if no match is found.
         //Use Case: Fetch a single record based on a specific condition, such as getting a product by ID or by name.
-        T Get(Expression<Func<T, bool>> filter);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
