d87c5ffbeeff042c9a1bc7ac9e4fa348d9ba80f5
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-10-06 22:44:08 +0300

    Get Shopping Cart

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -12,7 +12,7 @@ namespace Bulky.DataAccess.Repository.IRepository
         //Purpose: Retrieves all records from the data store for the given entity type.
         //Return Type: IEnumerable<T> represents a collection of T entities.
         //Use Case: Useful for returning all rows in a database table, such as getting a list of all products.
-        IEnumerable<T> GetAll(string? includeProperties = null);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null);
         //Purpose: Retrieves a single entity from the data store that matches the specified filter condition.
         //Parameter:Expression<Func<T, bool>> filter: Represents a LINQ expression used to filter records, such as product => product.Id == 5.
         //Return Type: T — Returns a single entity that matches the criteria or null if no match is found.
