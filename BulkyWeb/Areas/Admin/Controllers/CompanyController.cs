504d43dbd4a9010ae5cb335a096b7a03480ad51e
Henok Gebrehiwot <heni.gebrehiwot@gmail.com>
2024-10-11 21:10:53 +0300

    Add Authorization

--- BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
+++ BulkyWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -1,11 +1,12 @@
 ﻿using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    //[Authorize(Roles = SD.Role_Admin)]
+    [Authorize(Roles = SD.Role_Admin)]
     public class CompanyController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
